d06cd3fbc3cbb32f01692d1db0e995ab8f8afea3
Jayden <jayden8611@gmail.com>
2025-06-01 23:30:55 +0800

    feat: prepare and send payload to BE

--- src/api/populate.ts
+++ src/api/populate.ts
@@ -1,10 +1,41 @@
 // No shared FE/BE types for now, so this is a bit messy
-interface Programme {
+// This interfaces are from the backend
+export interface Programme {
     name: string;
     type: "major" | "secondMajor" | "minor";
 }
+interface PopulateResponse {
+    name: string;
+    type: "major" | "secondMajor" | "minor";
+    categorised: CategorisedModules;
+}
+interface CategorisedModules {
+    commonCore?: ModuleCondensed[];
+    unrestrictedElectives?: ModuleCondensed[];
+    coreEssentials?: ModuleCondensed[]; 
+    coreElectives?: ModuleCondensed[];
+    coreSpecials?: ModuleCondensed[];
+    coreOthers?: ModuleCondensed[];
 
-export async function populateModules(payload: Programme) {
+    constraints?: {
+        doubleCountModules?: ModuleCondensed[];
+        level1000Modules?: ModuleCondensed[];
+        level2000Modules?: ModuleCondensed[];
+        nonNUSModules?: ModuleCondensed[];
+        nonUniqueModules?: ModuleCondensed[];
+    };
+}
+type ModuleCondensed = Readonly<{
+  moduleCode: ModuleCode;
+  title: ModuleTitle;
+  semesters: number[];
+}>;
+type ModuleCode = string;
+type ModuleTitle = string;
+
+
+// API to backend to populate programs based on user selections
+export async function populateModules(payload: Programme[]): Promise<PopulateResponse[]> {
     const res = await fetch("http://localhost:3000/api/populate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
