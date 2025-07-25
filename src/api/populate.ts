09bfb66a093b48c2193d6226a4bdb14c9bbc5046
Matthias <matthiasyim1@gmail.com>
2025-07-26 03:09:01 +0800

    updated handleGenerateCourses to deal with the changed format of the payload received from the backend

--- src/api/populate.ts
+++ src/api/populate.ts
@@ -1,16 +1,13 @@
-import { 
-    Programme,
-    PopulatedProgramPayload
- } from "../types/shared/populator";
+import { BackendResponse, ProcessProgrammesRequest, ProcessProgrammesResponse } from "../types/shared-types";
 
 const backendURL = process.env.REACT_APP_BACKEND_URL;
 
 // API to backend to populate programs based on user selections
-export async function populateModules(payload: Programme[]): Promise<PopulatedProgramPayload[]> {
-    const res = await fetch(`${backendURL}/api/populate/`, {
+export async function populateModules(req: ProcessProgrammesRequest): Promise<BackendResponse<ProcessProgrammesResponse>> {
+    const res = await fetch(`${backendURL}/api/academic-plan/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
+        body: JSON.stringify(req),
     });
 
     if (!res.ok) throw new Error("Backend error");
